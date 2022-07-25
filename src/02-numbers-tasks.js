1857141eb6b52f2931639c72b6a4580fc7fa4adb
botino-k <lassmary@bk.ru>
2022-07-25 14:59:01 +0300

    feat: solve 02.1 task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -185,8 +185,8 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** (pow)) * (10 ** (pow));
 }
 
 /**
@@ -206,8 +206,11 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2 || n === 3) return true;
+  if (n <= 1 || n % 2 === 0 || n % 3 === 0) return false;
+  for (let i = 5; i * i <= n; i += 6) if (n % i === 0 || n % (i + 2) === 0) return false;
+  return true;
 }
 
 /**
@@ -225,8 +228,14 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  let num = +value;
+  if (num) {
+    num = +value;
+  } else {
+    num = def;
+  }
+  return num;
 }
 
 module.exports = {
