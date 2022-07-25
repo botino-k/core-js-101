a11caa58d5d029ca224bb938d26360cb4edfbeed
botino-k <lassmary@bk.ru>
2022-07-25 23:33:36 +0300

    feat: solve 03 task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -109,11 +109,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const pr = x1 * y1 + x2 * y2;
-  const mod = Math.sqrt(x1 ** 2 + x2 ** 2) * Math.sqrt(y1 ** 2 + y2 ** 2);
-  const angle = Math.acos(pr / mod);
-  return angle;
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
